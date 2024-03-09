971adf8de54f846dffa3f7ef50db084ea24e32fe
dmarcospro <davidmarcos.pro@outlook.fr>
2024-03-09 12:31:00 +0100

    setup vitest

---
+++ __tests__/header.test.tsx
@@ -0,0 +1,10 @@
+import { describe, it } from "vitest"
+
+describe("Header", () => {
+  it("should display at least one nav item", async () => {
+    // render(<Header />)
+    // await screen.findByText("Services")
+    // const itemsNav = screen.getAllByRole("link")
+    // expect(itemsNav.length).toBe(4)
+  })
+})
